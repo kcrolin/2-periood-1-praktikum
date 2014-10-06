060dd598c588c995776ba81499d98893b80ddbe3
kcrolin <carolin.karing@khk.ee>
2014-10-06 10:33:06 +0100

    lingid
    lingid

---
+++ 2ndpage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+  <head>
+    <title>1 praktikum | teine leht</title>
+    <meta charset="utf-8">
+	
+  </head>
+  
+  <body>
+   <h3>...kuni jõuame siia. </h3>
+<a href="index.php">Take me back pls</a>
+   </body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+  <head>
+    <title>1 praktikum | esileht</title>
+    <meta charset="utf-8">
+	
+  </head>
+  
+  <body>
+   <h2>Alustame siit!</h2>
+   <a href="2ndpage.php">and let's see where we end up</a>
+   </body>
+</html>
