c408bcef453e33864f90d938dc8e906c2ec96266
kcrolin <carolin.karing@khk.ee>
2014-10-06 20:20:12 +0100

    php link
    php link

--- 2ndpage.php
+++ 2ndpage.php
@@ -1,12 +1,17 @@
 <!DOCTYPE HTML>
   <head>
     <title>1 praktikum | teine leht</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="utf-8">
+
 	
   </head>
   
   <body>
+  <?php	$_GET;
+	print_r($_GET); ?> 	
+	
    <h3>...kuni jõuame siia. </h3>
-<a href="index.php">Take me back pls</a>
+<a href="index.php?name=c2rolin&age=classified&country=estonia">Take me back pls</a>
    </body>
 </html>
--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE HTML>
   <head>
     <title>1 praktikum | esileht</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="utf-8">
 	
   </head>
   
   <body>
-   <h2>Alustame siit!</h2>
-   <a href="2ndpage.php">and let's see where we end up</a>
+    <?php	$_GET;
+	print_r($_GET); 
+	
+	$link_file="2ndpage.php"; 
+	$age=40;?> 	
+	
+   <h1>Alustame siit!</h1>
+   
+   <a href="2ndpage.php?what-is-this=2">and let's see where we end up</a>
+   
+  <?php  echo "<a href='$link_file?age=$age'>PS! Saab ka niimoodi</a>";?>
+       <?php echo "<h2>" . $_GET["name"]. "</h2>";
+       echo "<h2>" . $_GET["age"] . "</h2>";	    
+	   echo "<h2>" . $_GET["country"] . "</h2>";	?>
    </body>
 </html>
+
